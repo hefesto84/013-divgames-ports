3d1955c530b7cb034752b5ade51410f92aa39d9e
dsanteugini <hefesto84@gmail.com>
2022-06-02 10:28:39 +0200

    steroid-port

--- steroid-port/Game/Systems/Ship/ShipSystem.cs
+++ steroid-port/Game/Systems/Ship/ShipSystem.cs
@@ -34,9 +34,11 @@ namespace steroid_port.Game.Systems.Ship
 
         public override void Update()
         {
+            ProcessMovement();
+            FixPosition();
+            
             _view.UpdateView(_currentPosition);
-            _currentPosition.X += 1;
-
+            
             if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
             {
                 Reset();
@@ -49,6 +51,19 @@ namespace steroid_port.Game.Systems.Ship
             
             _view = new ShipView(_renderService);
             _view.Init(_spriteService);
+        }
+
+        private void ProcessMovement()
+        {
+            _currentPosition.X += 1;
+        }
+        
+        private void FixPosition()
+        {
+            if (_currentPosition.Y > _screenService.CurrentSize.Y) _currentPosition.Y = 0;
+            if (_currentPosition.Y < 0) _currentPosition.Y = _screenService.CurrentSize.Y;
+            if (_currentPosition.X < 0) _currentPosition.X = _screenService.CurrentSize.X;
+            if (_currentPosition.X > _screenService.CurrentSize.X) _currentPosition.X = 0;
         }
     }
 }
