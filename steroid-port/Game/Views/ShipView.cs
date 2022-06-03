2f531cf2e7afcc724dd093c05c315aff531eeee3
dsanteugini <hefesto84@gmail.com>
2022-06-03 20:52:14 +0200

    Movement ship

--- steroid-port/Game/Views/ShipView.cs
+++ steroid-port/Game/Views/ShipView.cs
@@ -11,26 +11,28 @@ namespace steroid_port.Game.Views
     {
         private Tuple<Rectangle,Texture2D> _textureData;
         private Rectangle _destination;
+        private Vector2 _shipCenter;
 
         public ShipView(RenderService renderService) : base(renderService)
         {
+            
         }
         
         public void Init(SpriteService spriteService)
         {
             _textureData = spriteService.Get("ship");
             _destination = new Rectangle(0, 0, _textureData.Item1.width, _textureData.Item1.height);
+            _shipCenter = Vector2.Zero;
         }
         
         public void UpdateView(Vector2 position, int rotation)
         {
-            Console.WriteLine("RO: "+rotation);
-            
             _destination.x = position.X;
             _destination.y = position.Y;
+            _shipCenter.X = _destination.width * 0.5f;
+            _shipCenter.Y = _destination.height * 0.5f;
             
-            RenderService.Render(_textureData.Item2, _textureData.Item1, _destination, new Vector2(_destination.width/2, _destination.height/2), rotation);
-            //RenderService.Render(_textureData.Item2, _textureData.Item1, new Rectangle(position.X,position.Y,_textureData.Item1.width, _textureData.Item1.height));
+            RenderService.Render(_textureData.Item2, _textureData.Item1, _destination, _shipCenter, rotation);
         }
     }
 }
